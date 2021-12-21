838974ee130c7fc452b82cf3e47ee3c4be4838da
Aleksander M <aleksander.michalski222@gmail.com>
2021-12-21 09:40:13 +0100

    Initial commit

---
+++ src/pl/akademia108/Main.java
@@ -0,0 +1,17 @@
+package pl.akademia108;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age;
+        age = 23;
+        System.out.println(age);
+        double price=12.99;
+        System.out.println(price);
+        age = 30;
+        price = 15.99;
+        System.out.println("Wartości nadpisane");
+        System.out.println(age);
+        System.out.println(price);
+    }
+}
